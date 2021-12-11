8f1745d9a3dbc09d6da9bbe71019b17a0a2ae96c
Orphyux <Orphyux@10.0.0.38>
2021-12-10 20:02:15 -0500

    fixed it

--- edu.monmouth.book/src/edu/monmouth/book/BookConstants.java
+++ edu.monmouth.book/src/edu/monmouth/book/BookConstants.java
@@ -5,5 +5,9 @@ public class BookConstants {
 	public static final int MINTITLELEN = 1;
 	public static final double MINPRICE = 0.00;
 	public static final int MINPAGES = 1;
+	public static final int PAGESOFFSET = 2;
+	public static final int PRICEOFFSET = 3;
+	public static final int TITLEOFFSET = 0;
+	public static final int BOOKTYPEOFFSET = 1;
 
 }
